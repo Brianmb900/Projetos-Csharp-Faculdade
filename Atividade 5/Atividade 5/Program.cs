3ee1af4bb317b18e4afd8469b73a45acdc1837f2
Brian Melinski Bianchini <brian.bianchini@fatec.sp.gov.br>
2021-06-15 17:04:13 -0300

    Atividades para sábado

--- Atividade 5/Atividade 5/Program.cs
+++ Atividade 5/Atividade 5/Program.cs
@@ -9,7 +9,16 @@ namespace Atividade_5
     class Program
     {
         static void Main(string[] args)
-        {
-        }
+        {//Início
+            //#Desenvolvedor Brian Melinski Bianchini 15.06.2021
+
+            int a, b;
+            Console.WriteLine("Digite um número inteiro para seu antecessor ser exibido");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = a - 1;
+            Console.WriteLine("O antecessor é: " + b);
+            Console.ReadKey();
+
+        }//Fim
     }
 }
