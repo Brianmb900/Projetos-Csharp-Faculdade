b6ec23e9404d514940c8601063341b916336f5b5
Brian Melinski Bianchini <brian.bianchini@fatec.sp.gov.br>
2021-06-15 16:05:53 -0300

    Início do projeto

--- Projetinho 1/Projetinho console 1/Program.cs
+++ Projetinho 1/Projetinho console 1/Program.cs
@@ -12,14 +12,23 @@ namespace Projetinho_console_1
         {//Início
             //#Desenvolvedor Brian Melinski Bianchini 15.06.2021
 
-            double n1, n2, r;
-            Console.WriteLine("Calculadora de somas de 2 números");
-            Console.WriteLine("Digite o primerio número");
-            n1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite o segundo número");
-            n2 = Convert.ToDouble(Console.ReadLine());
-            r = n1 + n2;
-            Console.WriteLine("O Resultado é: " + r);
+            double n1, n2, rso, rsu, rm, rd;
+            Int16 o;
+            Console.WriteLine("Calculadora V1.0");
+            Console.WriteLine("Digite 1-Soma, 2-Subtração, 3-Multiplicação, 4-Divisão");
+            o = Convert.ToInt16(Console.ReadLine());
+            switch (o)
+            {
+                case 1:
+                    Console.WriteLine("Digite 2 números para somar");
+                    Console.WriteLine("Digite o 1º");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite o 2º");
+                    n2 = Convert.ToDouble(Console.ReadLine());
+                    rso = n1 + n2;
+                    Console.WriteLine("O resultado da soma é: " + rso);
+                    break;
+            }
             Console.ReadKey();
         }//Fim
     }
