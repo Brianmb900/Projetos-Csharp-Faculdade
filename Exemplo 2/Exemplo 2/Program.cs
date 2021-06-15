9aaf5e0a6c6790f9280f3ffe1466a591d7f66aba
Brian Melinski Bianchini <brian.bianchini@fatec.sp.gov.br>
2021-06-15 15:42:08 -0300

    Atualização do código

--- Exemplo 2/Exemplo 2/Program.cs
+++ Exemplo 2/Exemplo 2/Program.cs
@@ -13,11 +13,11 @@ namespace Exemplo_2
             //#Desenvolvedor Brian Melinski Bianchini 15.06.2021
 
             double n1, n2, r;
-            Console.WriteLine("Calculadora de somas de 2 números");//Só write não pula linha.
-            Console.WriteLine("Digite o primerio número");//WriteLine pula linha.
+            Console.WriteLine("Calculadora de somas de 2 números");//Só write escreve e não pula linha.
+            Console.WriteLine("Digite o primerio número");//WriteLine escreve e pula linha.
             n1 = Convert.ToDouble(Console.ReadLine());//Convert é usado para converter o valor para o memso da variável.
             Console.WriteLine("Digite o segundo número");
-            n2 = Convert.ToDouble(Console.ReadLine());
+            n2 = Convert.ToDouble(Console.ReadLine());//Console ReadLine é usado para ler o valor da linha e então pular uma linha, só read não pula a linha.
             r = n1 + n2;
             Console.WriteLine("O Resultado é: " + r);
             Console.ReadKey();
