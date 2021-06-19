bd163132e4627e029bc2cae3b8befbb55690dcd3
Brian Melinski Bianchini <brian.bianchini@fatec.sp.gov.br>
2021-06-19 09:58:56 -0300

    Melhoria de código e anotações

--- Atividade 2/Atividade 2/Program.cs
+++ Atividade 2/Atividade 2/Program.cs
@@ -12,20 +12,20 @@ namespace Atividade_2
         {//Início
             //#Desenvolvedor Brian Melinski Bianchini 15.06.2021
 
-            double n1, n2, rso, rsu, rsm, rsd;
+            double n1, n2, r;//Colocar os resultados em uma única variável para o programa ficar mais leve
             Console.WriteLine("Digite dois números para serem somados, subtraidos, multiplicados e divididos");
             Console.WriteLine("1º Num.");
             n1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("2º Num.");
             n2 = Convert.ToDouble(Console.ReadLine());
-            rso = n1 + n2;
-            rsu = n1 - n2;
-            rsm = n1 * n2;
-            rsd = n1 / n2;
-            Console.WriteLine("O resultado da soma é: " + rso);
-            Console.WriteLine("O resultado da subtração é: " + rsu);
-            Console.WriteLine("O resultado da multiplicação é: " + rsm);
-            Console.WriteLine("O resultado da divisão é: " + rsd);
+            r = n1 + n2;
+            Console.WriteLine("O resultado da soma é: " + r);
+            r = n1 - n2;
+            Console.WriteLine("O resultado da subtração é: " + r);
+            r = n1 * n2;
+            Console.WriteLine("O resultado da multiplicação é: " + r);
+            r = n1 / n2;
+            Console.WriteLine("O resultado da divisão é: " + r);
             Console.ReadKey();
 
         }//Fim
