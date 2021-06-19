736cd75ce8db680e2b5d32a660f4dda98be7f59b
Brian Melinski Bianchini <brian.bianchini@fatec.sp.gov.br>
2021-06-19 11:23:12 -0300

    Exemplo das funçõe matemáticas (Math)

---
+++ Exemplo 3/Exemplo 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Iníco
+            //#Desenvolvedor Brian Melinski Bianchini
+
+            double n, r, q;
+            Console.WriteLine("Insira um número para ser feita a raíz quadrada dele e também elevalo ao quadrado");
+            n = Convert.ToDouble(Console.ReadLine());
+            r = Math.Sqrt(n);
+            q = Math.Pow(n, 2);
+            Console.WriteLine("A raiz é: " + r);
+            Console.WriteLine("O quadrado é: " + q);
+            Console.ReadKey();
+
+        }//Fim
+    }
+}
