3fb9c5de9056e44ca66ae710c2f46e46b7d1d5b4
Brian Melinski Bianchini <brian.bianchini@fatec.sp.gov.br>
2021-06-20 15:21:49 -0300

    Atualizações

--- Aula 4/Atividades/Atividade 3/Atividade 3/Program.cs
+++ Aula 4/Atividades/Atividade 3/Atividade 3/Program.cs
@@ -15,7 +15,7 @@ namespace Atividade_3
             string n, pn, ao, ns;
             Console.WriteLine("Digite seu nome completo");
             n = Console.ReadLine();
-            pn = n.Substring(5,6);
+            pn = n.Substring(5,6);// A função substring corta um pedaço da string começando em um caractere específico, e vai até x-1 caracteres adiante. Exemplo: paralelepipedo SubString(2,3) = ral caracatere inicial 2 = r até 3-1 =l resultando no ral.
             ao = n.Replace("a", "o");
             Console.Write($"Nome e sobrenome separado: ");
             Console.WriteLine($"O pedaço do nome do caractere 5 ao 10 é: {pn}");
