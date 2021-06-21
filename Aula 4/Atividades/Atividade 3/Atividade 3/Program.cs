3135e1a292ff52b725e7ab72eed4583061124936
Brian Melinski Bianchini <brian.bianchini@fatec.sp.gov.br>
2021-06-21 10:49:46 -0300

    Atividades para entregar terça 22/06/2021

--- Aula 4/Atividades/Atividade 3/Atividade 3/Program.cs
+++ Aula 4/Atividades/Atividade 3/Atividade 3/Program.cs
@@ -11,17 +11,24 @@ namespace Atividade_3
         static void Main(string[] args)
         {//Início
             //# Desenvolvedor Brian Melinski Bianchini 20.06.2021
-
-            string n, pn, ao, ns;
+            string n, pn, ao, ns, s;
+            int le, le2, l1, lc, u;
             Console.WriteLine("Digite seu nome completo");
             n = Console.ReadLine();
             pn = n.Substring(5,6);// A função substring corta um pedaço da string começando em um caractere específico, e vai até x-1 caracteres adiante. Exemplo: paralelepipedo SubString(2,3) = ral caracatere inicial 2 = r até 3-1 =l resultando no ral.
             ao = n.Replace("a", "o");
-            Console.Write($"Nome e sobrenome separado: ");
-            Console.WriteLine($"O pedaço do nome do caractere 5 ao 10 é: {pn}");
-            Console.WriteLine($"A substituição de a por o é: {ao}");
+            le = n.IndexOf(" ");
+            le2 = n.LastIndexOf(" ");
+            l1 = (le2 + 1);
+            u = n.Length;
+            lc = ((u - le2) - 1);
+            ns = n.Substring(0,le);
+            s = n.Substring(l1,lc);
+            Console.WriteLine($"Nome:{ns}");
+            Console.WriteLine($"Sobrenome:{s}");
+            Console.WriteLine($"O pedaço do nome do caractere 5 ao 10 é:{pn}");
+            Console.WriteLine($"A substituição de a por o é:{ao}");
             Console.ReadLine();
-
         }//Fim
     }
 }
---
+++ Aula 4/Atividades/Atividade 4/Atividade 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Início
+            //# Desenvolvedor Brian Melinski Bianchini 21.06.2021
+
+            string n, ns, e, en;
+            int le;
+            Console.WriteLine("Digite seu nome completo");
+            n = Console.ReadLine();
+            le = n.IndexOf(" ");
+            ns = n.Substring(0, le);
+            e = "nome@fatec.sp.gpv.br";
+            ns = ns.ToLower();
+            en = e.Replace("nome", ns);
+            Console.WriteLine($"Seu e-mail institucional é:{en}");
+            Console.ReadLine();
+        }//Fim
+    }
+}
