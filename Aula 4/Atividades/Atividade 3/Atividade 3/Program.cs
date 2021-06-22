f65fc63b7c897118b4b7703f5f4b6212e6e89d88
Brian Melinski Bianchini <brian.bianchini@fatec.sp.gov.br>
2021-06-22 14:32:14 -0300

    Versão do professor adicionada como comentário

--- Aula 4/Atividades/Atividade 3/Atividade 3/Program.cs
+++ Aula 4/Atividades/Atividade 3/Atividade 3/Program.cs
@@ -11,6 +11,7 @@ namespace Atividade_3
         static void Main(string[] args)
         {//Início
             //# Desenvolvedor Brian Melinski Bianchini 20.06.2021
+
             string n, pn, ao, ns, s;
             int le, le2, l1, lc, u;
             Console.WriteLine("Digite seu nome completo");
